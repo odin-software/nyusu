16d581fc990abe4c69b8fbf8d5ce2a1a18ae1d28
Kendry Grullon <kengrullon@gmail.com>
2024-08-15 11:22:20 -0400

    deleting frontend, trying for a new approach

--- frontend/src/types.ts
+++
@@ -1,7 +0,0 @@
-export type Post = {
-  id: number;
-  name: string;
-  title: string;
-  url: string;
-  published_at: number;
-};
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -13,13 +14,36 @@ func main() {
 	cfg := server.NewConfig()
 	ticker := time.NewTicker(time.Duration(cfg.Env.Scrapper) * time.Second)
 
+	// GET localhost:8888/v1/readiness, cfg.Readiness OK 200, Bad Request 400, Not Found 404
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /v1/readiness", cfg.Readiness)
 	mux.HandleFunc("GET /v1/err", cfg.Err)
 
-	mux.HandleFunc("/v1/users/login", server.CORS(cfg.LoginUser))                 // post
-	mux.HandleFunc("/v1/users/register", server.CORS(cfg.RegisterUser))           // post
-	mux.HandleFunc("/v1/users", server.CORS(cfg.MiddlewareAuth(cfg.GetAuthUser))) // get
+	// Page endpoints.
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("html/index.html")
+		if err != nil {
+			panic(err)
+		}
+		err = t.Execute(w, "")
+		if err != nil {
+			panic(err)
+		}
+	})
+	mux.HandleFunc("GET /login", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("html/login.html")
+		if err != nil {
+			panic(err)
+		}
+		err = t.Execute(w, "")
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	mux.HandleFunc("POST /v1/users/login", server.CORS(cfg.LoginUser))                // post
+	mux.HandleFunc("POST /v1/users/register", server.CORS(cfg.RegisterUser))          // post
+	mux.HandleFunc("GET /v1/users", server.CORS(cfg.MiddlewareAuth(cfg.GetAuthUser))) // get
 
 	mux.HandleFunc("GET /v1/feeds", server.CORS(cfg.GetAllFeeds))                     // get
 	mux.HandleFunc("POST /v1/feeds", server.CORS(cfg.MiddlewareAuth(cfg.CreateFeed))) // post
@@ -33,9 +57,9 @@ func main() {
 	mux.HandleFunc("DELETE /v1/posts/bookmarks/{postId}", server.CORS(cfg.MiddlewareAuth(cfg.UnbookmarkPost))) // delete
 	mux.HandleFunc("POST /v1/posts/bookmarks/{postId}", server.CORS(cfg.MiddlewareAuth(cfg.BookmarkPost)))     // post
 	mux.HandleFunc("OPTIONS /v1/posts/bookmarks/{postId}", server.OPTIONS)                                     // options
-	mux.HandleFunc("/v1/posts/bookmarks", server.CORS(cfg.MiddlewareAuth(cfg.GetBookmarkedPosts)))             // get
-	mux.HandleFunc("/v1/posts/{feedId}", server.CORS(cfg.MiddlewareAuth(cfg.GetPostByUsersAndFeed)))           // get
-	mux.HandleFunc("/v1/posts", server.CORS(cfg.MiddlewareAuth(cfg.GetPostByUsers)))                           // get
+	mux.HandleFunc("GET /v1/posts/bookmarks", server.CORS(cfg.MiddlewareAuth(cfg.GetBookmarkedPosts)))         // get
+	mux.HandleFunc("GET /v1/posts/{feedId}", server.CORS(cfg.MiddlewareAuth(cfg.GetPostByUsersAndFeed)))       // get
+	mux.HandleFunc("GET /v1/posts", server.CORS(cfg.MiddlewareAuth(cfg.GetPostByUsers)))                       // get
 	// server.TestRssParsing("https://frontendmasters.com/blog/feed/")
 	// server.TestRssParsing("https://triss.dev/blog/rss.xml")
 
